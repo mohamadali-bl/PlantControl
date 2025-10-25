7ffbfda305f3d317f06c9e7d6cf5b899108a940d
mohamadali313 <mr.mohamadali.balvayeh@gmail.com>
2025-10-25 17:37:25 +0330

    Update and rename Plant.hpp to PlantHeader.hpp

---
+++ src/PlantHeader.hpp
@@ -0,0 +1,58 @@
+//*********************************************************//
+#ifndef PLANTHEADER_H
+#define PLANTHEADER_H
+#include <Arduino.h>
+#include <RTClib.h>            //RTC
+#include <LiquidCrystal_I2C.h> //LCD
+#include <Wire.h>              //I2C LCD
+
+/*
+ * 
+*/
+
+//Constants----------------------------------------------------------------//
+extern RTC_DS1307 RTC;
+extern LiquidCrystal_I2C LCD;
+#define MC PlantClass //MyClass
+
+//-----------------------------------Class---------------------------------//
+class PlantClass {
+  private:
+    //Pins
+    uint8_t PumpPin, DIYSensorPin, LightPin;
+    //Values
+    unsigned int DIYSensorValue = 0;
+    unsigned long now, pumpStartTime = 0, lastLCDPrint = 0, lastLighting = 0, lastSerialPrint = 0;
+    char daysOfWeek[7][5] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"}; 
+    //Controller Functions
+    void Pump(); //Pump Controller
+    void Light(), Lighting(bool); bool LightingTimeLine(); //Light Controller
+    //Set Objects
+    void RTC_Set(), LCD_Set();
+    //Object
+    LiquidCrystal_I2C &lcd;
+    RTC_DS1307 &rtc; DateTime Time;
+  public:
+    //Flag
+    bool pumpFlag, lightFlag, rtcFlag;
+    //Class Function
+    PlantClass(uint8_t Ppin, uint8_t DIYpin, uint8_t Lpin, LiquidCrystal_I2C &lcd_ref, RTC_DS1307 &rtc_ref) :
+    PumpPin(Ppin), DIYSensorPin(DIYpin), LightPin(Lpin), lcd(lcd_ref), rtc(rtc_ref) {pumpFlag = false; lightFlag = false; rtcFlag = false;}
+    void Main();
+    //Setup
+    void begin() { RTC_Set(); LCD_Set(); 
+      pinMode(PumpPin, OUTPUT);   pinMode(LightPin, OUTPUT);
+      digitalWrite(PumpPin, LOW); digitalWrite(LightPin, LOW);
+    } 
+    //Print Functions
+    void Seriall();      //Serial Print (json)
+    void Humidity_LCD(); //Humidity Sensor on LCD
+    void LCD();          //Print LCD
+    void Clock(), Calendar(), DayOfWeek(); //RTC Time
+    bool LcdTimeLine();  //Change Print Shape
+    void Space(){lcd.print(F("                        "));} //24 NULL space
+    void Clear(uint8_t sa, uint8_t so) {lcd.setCursor(sa, so); Space();}
+};
+
+#endif //PLANTHEADER_H
+//*********************************************************//
